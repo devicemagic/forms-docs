69fe6db1ff4093a86c329eca5b7a653343d746b1
Dusan Babich <dusanb@devicemagic.com>
2011-05-20 17:42:16 +0200

    Disabled broken search, some more edits too.

--- toc.rb
+++ toc.rb
@@ -1,29 +1,4 @@
-section 'getting-started', "Getting Started" do
-	topic 'heroku',          'What is Heroku?'
-	topic 'quickstart',      'Quickstart guide'
-	topic 'keys',            'Managing keys'
-	topic 'collab',          'Collaborator quickstart'
-	topic 'billing',         'Confirmation & verification'
-	topic 'windows',         'Ruby on Windows'
-	topic 'rails',           'Running Rails apps'
-end
-
 section 'api', "API" do
   topic 'http-connector', "HTTP Connector"
   topic 'push-api',       "Push API"
 end   
-
-section 'howto', "How To" do
-	topic 'background-jobs',    'Background jobs'
-	topic 'full-text-search',   'Full text search'
-	topic 'smtp',               'Outbound email'
-	topic 's3',                 'Storing uploads in S3'
-	topic 'custom-domains',     'DNS & custom domains'
-end
-
-section 'faq', "FAQ" do
-	topic 'faq-accounts-billing',   'Accounts & billing'
-	topic 'faq-product-features',   'Product features'
-	topic 'faq-support'         ,   'Support'
-end
-
